23113f55da81afe923dfeabb306e1c875c074b92
Jerwin Manongsong <61395841+nahadgur@users.noreply.github.com>
2026-02-10 22:26:15 +0800

    Update index.ts

--- content/blog/index.ts
+++ content/blog/index.ts
@@ -3,12 +3,14 @@ import type { RawPostFile } from './rentLeases'
 import { rentLeases } from './rentLeases'
 import { buildingIssues } from './buildingIssues'
 import { safetySecurity } from './safetySecurity'
+import { buildingViolations } from './buildingViolations'
 
 export type { RawPostFile }
-export { rentLeases, buildingIssues, safetySecurity }
+export { rentLeases, buildingIssues, safetySecurity, buildingViolations }
 
 export const allRawPosts: RawPostFile[] = [
   ...rentLeases,
   ...buildingIssues,
   ...safetySecurity,
+  ...buildingViolations,
 ]
